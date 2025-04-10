ffd93cc7a2a9c3f103e95133171ae999abb29938
Will Sargent <will.sargent@gmail.com>
2025-04-10 08:57:31 -0700

    Add simple test

---
+++ hayhooks/tests/test_example.py
@@ -0,0 +1,25 @@
+from haystack.components.routers import ConditionalRouter
+from typing import List
+
+def test_conditional_router():
+    """Test conditional router works."""
+
+    routes = [
+        {
+            "condition": "{{streams|length > 2}}",
+            "output": "{{streams}}",
+            "output_name": "enough_streams",
+            "output_type": List[int],
+        },
+        {
+            "condition": "{{streams|length <= 2}}",
+            "output": "{{streams}}",
+            "output_name": "insufficient_streams",
+            "output_type": List[int],
+        },
+    ]
+    router = ConditionalRouter(routes)
+    kwargs = {"streams": [1, 2, 3], "query": "Haystack"}
+    result = router.run(**kwargs)
+
+    assert result["enough_streams"] == [1,2,3]
