29cd17f41cc4793da182b25ae8eb0c4f757699cd
Will Sargent <will.sargent@gmail.com>
2025-04-11 07:02:18 -0700

    Add content extractor test

---
+++ hayhooks/tests/components/test_content_extraction.py
@@ -0,0 +1,26 @@
+from haystack import Pipeline, Document
+
+from components.content_extraction import build_content_extraction_component
+
+def test_build_content_extraction_component():
+    """
+    Test that the content extraction component can be built and has the expected structure.
+    """
+    extraction_component = build_content_extraction_component()
+
+    pipe = Pipeline()
+    pipe.add_component("extractor", extraction_component)
+    assert "extractor" in pipe.graph.nodes
+
+def test_content_extraction_component_run():
+    """
+    Test that we can run the extractor and get content
+    """
+    extraction_component = build_content_extraction_component()
+    result = extraction_component.run(urls=["http://example.com"])
+
+    assert "documents" in result
+    assert isinstance(result["documents"], list)
+    assert len(result["documents"]) > 0
+    assert isinstance(result["documents"][0], Document)
+
