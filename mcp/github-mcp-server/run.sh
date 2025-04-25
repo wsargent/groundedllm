af5b78714d9df326eeea1c25c2be0e3396c31819
Will Sargent <will.sargent@gmail.com>
2025-04-24 19:35:34 -0700

    Delete the MCP servers

--- mcp/github-mcp-server/run.sh
+++
@@ -1,6 +0,0 @@
-
-export GITHUB_DYNAMIC_TOOLSETS=1
-export GITHUB_PERSONAL_ACCESS_TOKEN=$(op read "op://Private/Github Personal Access Token Vault/token")
-mcp-proxy --sse-port=8080 \
-  --pass-environment \
-  -- ./github-mcp-server stdio
